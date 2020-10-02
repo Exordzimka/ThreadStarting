3033355b22381c26a4745c8d0a01d267af0ad7a5
Dzimka <exordzimka@gmail.com>
2020-10-03 01:09:07 +0300

    Initial commit

---
+++ ThreadStart/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ThreadStart
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Заполнить Массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(102, 10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(54, 433);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Buble";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(251, 11);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(54, 433);
+            this.listBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(309, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 20);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Shell";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(400, 11);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(54, 433);
+            this.listBox3.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(459, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 20);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Insert";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(549, 11);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(54, 433);
+            this.listBox4.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(10, 377);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 63);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Запустить ВСЕ сортировки";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 451);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
+
---
+++ ThreadStart/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadStart
+{
+    public partial class Form1 : Form
+    {
+        private int[] _array;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void FillingDelegate(ListBox listBox, int[] array);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillingArray(int.Parse(textBox1.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BubbleSort();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShellSort();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            InsertSort();
+        }
+
+        private void FillingArray(int size)
+        {
+            _array = new int[size];
+            Random random = new Random();
+            listBox1.Items.Clear();
+            for (int i = 0; i < size; i++)
+            {
+                _array[i] = random.Next(10, 100);
+                listBox1.Items.Add(_array[i]);
+            }
+        }
+
+        private void BubbleSort()
+        {
+            var tempArray = (int[])_array.Clone();
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                var stop = true;
+                for(var j=0;j< tempArray.Length-1;j++)
+                {
+                    if (tempArray[j + 1] >= tempArray[j]) continue;
+                    var temp = tempArray[j];
+                    tempArray[j] = tempArray[j + 1];
+                    tempArray[j + 1] = temp;
+                    stop = false;
+                }
+                if (stop)
+                    break;
+            }
+            
+            FillingList(listBox2, tempArray);
+        }
+
+        private void InsertSort()
+        {
+            int[] tempArray = (int[])_array.Clone();
+
+            for (int i = 0; i < tempArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (tempArray[j - 1] > tempArray[j])
+                    {
+                        int temp = tempArray[j - 1];
+                        tempArray[j - 1] = tempArray[j];
+                        tempArray[j] = temp;
+                    }
+                }
+            }
+            
+            FillingList(listBox4, tempArray);
+        }
+
+        private void ShellSort()
+        {
+            var tempArray = (int[])_array.Clone();
+            int step;
+            for (step = tempArray.Length / 2; step > 0; step /= 2)
+            {
+                int i;
+                for (i = step; i < tempArray.Length; i++)
+                {
+                    var tmp = tempArray[i];
+                    int j;
+                    for (j = i; j >= step; j -= step)
+                    {
+                        if (tmp < tempArray[j - step])
+                            tempArray[j] = tempArray[j - step];
+                        else
+                            break;
+                    }
+                    tempArray[j] = tmp;
+                }
+            }
+           
+            FillingList(listBox3, tempArray);
+        }
+
+        private void FillingList(ListBox listBox, int[] array)
+        {
+            Action action = () =>
+            {
+                Filling(listBox, array);
+            };
+            
+            if(InvokeRequired)
+            {
+                Invoke(action);
+                //BeginInvoke(Filling(listBox, array));
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private static void Filling(ListBox listBox, int[] array)
+        {
+            listBox.Items.Clear();
+            foreach (var item in array)
+            {
+                listBox.Items.Add(item);
+            }
+        }
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var thread1 = new Thread(BubbleSort);
+            var thread2 = new Thread(InsertSort);
+            var thread3 = new Thread(ShellSort);
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+        
+    }
+}
